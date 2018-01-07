f5a033babc99361b56e47291a67aeae705fa88f3
Steven Harrison <steven.harrison@gmail.com>
2018-01-07 13:04:05 -0500

    Maps and geolocate works (at least in staging)

--- static/js/map.js
+++ static/js/map.js
@@ -1,6 +1,11 @@
+function initialize() {
+  initMap();
+  initAutocomplete();
+}
+
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    zoom: 14,
+    zoom: 8,
     center: {lat: -34.397, lng: 150.644}
   });
   var geocoder = new google.maps.Geocoder();
