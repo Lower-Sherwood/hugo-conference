6cd39d4606728ebb70243981bda17e54e3032f75
Steven Harrison <steven.harrison@gmail.com>
2018-01-07 02:05:10 -0500

    first pass at a registration page

---
+++ static/js/map.js
@@ -0,0 +1,24 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    zoom: 14,
+    center: {lat: -34.397, lng: 150.644}
+  });
+  var geocoder = new google.maps.Geocoder();
+
+  geocodeAddress(geocoder, map);
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('map-canvas').getAttribute("data-address");
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      console.log("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
