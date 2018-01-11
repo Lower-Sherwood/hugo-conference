98800f819c0a4fe1e1b2841bafd5b4f9e2268d1e
Steven Harrison <steven.harrison@gmail.com>
2018-01-10 23:16:23 -0500

    still working on smooth scrolling

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,8 +1,7 @@
 $('ul.nav').find('a').click(function() {
     var $href = $(this).attr('href');
-    //var $anchor = $('#'+$href).offset();
     $('html, body').animate({
-        scrollTop: $($href).offset().top
-    }, 'slow');
+        scrollTop: $($href).position().top
+    }, 'slow')
     return false;
 });
